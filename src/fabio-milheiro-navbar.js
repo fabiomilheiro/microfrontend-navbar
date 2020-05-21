8f900fdacaaa605c01c89eea63d60d85150aeac1
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-05-21 10:43:12 +0100

    Fixed domElementGetter.

--- src/fabio-milheiro-navbar.js
+++ src/fabio-milheiro-navbar.js
@@ -8,7 +8,7 @@ const lifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: Root,
-  domElementGetter: document.getElementById("navbar"),
+  domElementGetter: () => document.getElementById("navbar"),
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
     return null;
