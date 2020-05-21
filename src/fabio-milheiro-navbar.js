f11fa53474918abbc9caad6f079d91233fb1d83b
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-05-21 10:32:09 +0100

    Refactored domElementGetter to use an id instead.

--- src/fabio-milheiro-navbar.js
+++ src/fabio-milheiro-navbar.js
@@ -4,21 +4,11 @@ import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
 
-const getNavbarElement = () => {
-  var navbar = document.getElementsByClassName("navbar");
-
-  if (navbar.length == 1) {
-    return navbar[0];
-  }
-
-  return undefined;
-};
-
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: Root,
-  domElementGetter: getNavbarElement,
+  domElementGetter: document.getElementsById("navbar"),
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
     return null;
