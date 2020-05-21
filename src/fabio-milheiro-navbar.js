bbbd3b4a9e70fe5bfa837bba18c9163648200d3d
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-05-21 10:04:59 +0100

    Added domElementGetter.

--- src/fabio-milheiro-navbar.js
+++ src/fabio-milheiro-navbar.js
@@ -4,10 +4,21 @@ import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
 
+const getNavbarElement = () => {
+  var navbar = document.getElementsByClassName("navbar");
+
+  if (navbar.length == 1) {
+    return navbar[0];
+  }
+
+  return undefined;
+};
+
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: Root,
+  domElementGetter: getNavbarElement,
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
     return null;
