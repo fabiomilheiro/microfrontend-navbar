f95b57a0ab96fbe0cee487e8cd3f9cb207e66b32
Fabio Milheiro <fabiomilheiro@gmail.com>
2020-05-21 10:04:29 +0100

    Added BrowserRouter.

--- src/root.component.js
+++ src/root.component.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { Link, BrowserRouter } from "react-router-dom";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <BrowserRouter>
+      <section>
+        {props.name} is mounted!
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </section>
+    </BrowserRouter>
+  );
 }
